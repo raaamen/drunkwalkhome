26feca6168bf71f6cd295fe77af911a1367f9a44
sarah <sarahapplebaum.sia@gmail.com>
2023-11-13 21:46:06 -0500

    dialogue works

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Conversation testconvo;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        DialogueBoxManager.Instance.StartDialogue(testconvo);
     }
 
     // Update is called once per frame
