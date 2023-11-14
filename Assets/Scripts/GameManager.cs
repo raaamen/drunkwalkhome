529606b76815eed91a5cf4eef32472d06c7c4a50
sarah <sarahapplebaum.sia@gmail.com>
2023-11-13 22:04:10 -0500

    main menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,12 +10,15 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DialogueBoxManager.Instance.StartDialogue(testconvo);
+        //DialogueBoxManager.Instance.StartDialogue(testconvo);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void LoadScene(string sceneName){
+        SceneManager.LoadScene(sceneName);
     }
 }
