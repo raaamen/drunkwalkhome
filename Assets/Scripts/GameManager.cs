7a12d0bd1a72d325b2f6127dfc53b4eb5270d705
sarah <sarahapplebaum.sia@gmail.com>
2023-11-14 22:31:32 -0500

    test scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,7 +10,6 @@ public class GameManager : Singleton<GameManager>
     public int health;
     public bool home;
     public DisplayObject interactBox;
-
     public Conversation testconvo;
 
     // Start is called before the first frame update
@@ -22,9 +21,11 @@ public class GameManager : Singleton<GameManager>
     // Update is called once per frame
     void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.C)){
             DialogueBoxManager.Instance.StartDialogue(testconvo);
         }
+        */
     }
     public void LoadScene(string sceneName){
         SceneManager.LoadScene(sceneName);
